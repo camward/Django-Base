85d4c4ce9e85bf042377a07a508ddbb9fed976f3
alex <camward@yandex.ru>
2020-06-28 17:01:56 +0400

    модели

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
+
