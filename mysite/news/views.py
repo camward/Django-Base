6a76f64a093577dc4a177a94505df34a6500befc
alex <camward@yandex.ru>
2020-06-28 19:12:37 +0400

    шаблоны

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -3,11 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello world')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context=context)
 
 
 def test(request):
