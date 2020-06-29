ea4e534711baaf0373ef7701c5c04bf5f274ac7c
alex <camward@yandex.ru>
2020-06-29 22:28:40 +0400

    настройка админки создать суперпользователя - python3 manage.py createsuperuser

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import News
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+
+admin.site.register(News, NewsAdmin)
--- mysite/news/apps.py
+++ mysite/news/apps.py
@@ -6,3 +6,4 @@ from django.apps import AppConfig
 
 class NewsConfig(AppConfig):
     name = 'news'
+    verbose_name = 'Новости'
