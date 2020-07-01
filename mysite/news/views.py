a73c5b769af6825502391685ce1584ecce480399
alex <camward@yandex.ru>
2020-07-01 22:18:24 +0400

    класс DetailView

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import News, Category
 from .forms import NewsForm
@@ -38,6 +38,14 @@ class NewsByCategory(ListView):
     def get_queryset(self):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
 
+
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+    # template_name = 'news/news_detail.html'
+    # pk_url_kwarg = 'news_id'
+
+
 # def index(request):
 #     news = News.objects.all()
 #     context = {
@@ -47,10 +55,10 @@ class NewsByCategory(ListView):
 #     return render(request, template_name='news/index.html', context=context)
 
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'category': category})
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.get(pk=category_id)
+#     return render(request, 'news/category.html', {'news': news, 'category': category})
 
 
 def view_news(request, news_id):
