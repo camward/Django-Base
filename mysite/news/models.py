f6bcd96024d843ad2d5fbd867cb98258cb041706
alex <camward@yandex.ru>
2020-07-01 13:48:34 +0400

    обратное разрешение адресов

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.urls import reverse
 
 
 class News(models.Model):
@@ -12,6 +13,9 @@ class News(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={"news_id": self.pk})
 
     def __str__(self):
         return self.title
@@ -24,6 +28,9 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"category_id": self.pk})
 
     def __str__(self):
         return self.title
